50546a72c2638c8bc647da59877864a2c317bdbf
Adrian <adgarcia1119@gmail.com>
2021-11-09 04:16:54 -0500

    enable inline reply with JSON data

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-const Discord = require('discord.js');
-
+import Discord, { ReactionCollector } from 'discord.js';
+import 'discord-reply';
 const client = new Discord.Client();
 
-const config = require('./config.json');
+import fetch from 'node-fetch';
+import { readFile } from 'fs/promises';
+
+const config = JSON.parse(
+  await readFile(
+    new URL('./config.json', import.meta.url)
+  )
+);
 
 client.login(config.token);
 
-const fs = require('fs');
-
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
+import fs from 'fs';
 
 client.once('ready', () => {
     console.log('Ready to go!');
 })
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if(command === 'flip'){
-        client.commands.get('flip').execute(message, args);
+async function getData(url) {
+    let oembed = `http://www.tiktok.com/oembed/?url=${url}`;
+    console.log("oembed link", oembed, '\n');
+    
+    let dataFromOembed = await fetch(oembed);
+    let data = await dataFromOembed.json();
+    if (data === undefined) {
+        throw Error("No data received from oembed");
     }
 
-});
+    //console.log("data from oembed", data, '\n');
+
+    return data;
+}
+
+client.on('message', message =>{
+    if(!message.content.includes(".tiktok.com/") || message.author.bot) return;
+
+    let data = getData(message.content);
+
+    data.then(function(result) {
+        console.log(result)     //console.log(data) will print: Promise { <pending> }
+        message.lineReply('```json\n' + JSON.stringify(result) + '\n```');
+    })
+
+
+// const args = message.content.slice(prefix.length).split(/ +/);
+    // const command = args.shift().toLowerCase();
+
+    // if(command === 'flip'){
+    //     client.commands.get('flip').execute(message, args);
+    // }
+ 
+},
+
+);
