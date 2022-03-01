9e2c9fc7d2b61aff6856f0ae35429712dfd3800f
Adrian <adgarcia1119@gmail.com>
2022-03-01 06:51:21 -0500

    selenium webdriver

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const {Client, MessageAttachment} = require('discord.js');
+const client = new Client();
 
 require('dotenv').config();
 require('discord-reply');
+var webdriver = require('selenium-webdriver');
+var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 const axios = require('axios');
-const cheerio = require('cheerio');
 const fs = require('fs');
+
+//*[@id="app"]/div[2]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/div/video
+///html/body/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/div/video
+
 
 client.login(process.env.TOKEN);
 
@@ -13,21 +18,18 @@ client.once('ready', () => {
     console.log('Ready to go!');
 })
 
-//my user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36
-
-var config = {headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}};
-
 client.on('message', message =>{
     if(!message.content.includes(".tiktok.com/") || message.author.bot) return;
+    
+    driver.get(message.content);
+    driver.wait(webdriver.until.elementLocated(webdriver.By.xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/div/video')), 10000);
+    let element = driver.findElement(webdriver.By.xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/div/video'));
+    
+    element.getAttribute('src')
+        .then(function(url){
+            //const attachment = new MessageAttachment(url)
 
-    axios.get(message.content)
-        .then((response) => {  
-            console.log(response);
-        })
-        .catch((error) => {
-            console.error(error)
+            message.lineReply(url); 
         });
-
-    message.lineReply('lol');   
 }
 );
--- test.js
+++
@@ -1,8 +0,0 @@
-const request = require('request');
-// const test = new Request('https://www.tiktok.com/@_mrforgetable/video/7063596664170335534?_t=8PqbYUHBm62&_r=1')
-
-request('https://vm.tiktok.com/TTPdBk4Nyj/', function(error, response, html){
-    console.error('error:', error);
-    console.log('statusCode: ', response && response.statusCode);
-    console.log('html: ', html);
-});
