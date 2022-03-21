a8bd82ada192049077571dec92cc1800a8b9a814
Adrian <adgarcia1119@gmail.com>
2022-03-21 12:47:40 -0400

    reduce static wait time

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ client.on('message', async message =>{
         await stream.on('end', () => {
             output.end();
             const attachment = new MessageAttachment('\output.mp4');
-            message.lineReply("test:", attachment)
+            message.lineReply("", attachment)
                 .catch((error)=>{
                     console.log('Caught: ', error.name, error.message)
                     message.lineReply('Sorry. File is larger than Discord\'s 8MB Limitation.')
