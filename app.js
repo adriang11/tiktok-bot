7d446993cb70cccc575f23319094f336f366faa3
Adrian <adgarcia1119@gmail.com>
2022-05-03 05:01:33 -0400

    remove options

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const fs = require('fs');
 
 let options = new Options();
 options.headless()
-options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH)
-options.setBrowserVersion(process.env.CHROMEDRIVER_VERSION)
-options.addArguments('--disable-gpu'); //Disables GPU hardware acceleration. If software renderer is not in place, then the GPU process won't launch.
-options.addArguments('--no-sandbox'); //Disables the sandbox. The Google sandbox is a development and test environment for developers working on Google Chrome browser-based applications. Disabling this to run on heroku
+// options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH)
+// options.setBrowserVersion(process.env.CHROMEDRIVER_VERSION)
+// options.addArguments('--disable-gpu'); //Disables GPU hardware acceleration. If software renderer is not in place, then the GPU process won't launch.
+// options.addArguments('--no-sandbox'); //Disables the sandbox. The Google sandbox is a development and test environment for developers working on Google Chrome browser-based applications. Disabling this to run on heroku
 options.addArguments('--disable-dev-shm-usage'); //This will force Chrome to use the /tmp directory instead. Fixing issue with tab crashing due to Heroku attempting to always use /dev/shm for non-executable memory.
 
 client.login(process.env.TOKEN);
