538ff05a2fa3eafb3dfc2fd9c4bb1ba94e11e70f
Adrian <adgarcia1119@gmail.com>
2022-05-03 04:45:33 -0400

    reset build cache

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ const fs = require('fs');
 
 let options = new Options();
 options.headless()
-// options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH)
-// options.addArguments('--headless');
+options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH)
 options.addArguments('--disable-gpu'); //Disables GPU hardware acceleration. If software renderer is not in place, then the GPU process won't launch.
 options.addArguments('--no-sandbox'); //Disables the sandbox. The Google sandbox is a development and test environment for developers working on Google Chrome browser-based applications. Disabling this to run on heroku
 options.addArguments('--disable-dev-shm-usage'); //This will force Chrome to use the /tmp directory instead. Fixing issue with tab crashing due to Heroku attempting to always use /dev/shm for non-executable memory.
@@ -73,7 +72,7 @@ client.on('message', async message =>{
             message.lineReply("Element Not Found On Page");
           } else {
             console.log('Caught: ', error.name, error.message)
-            message.lineReply(error.message.toString());
+            message.lineReply(error.name.toString());
           }
     } finally {
         await driver.quit();
